8f26295759ad57818a9ccc65c6ee26db633dc844
Bak <shaia.bak@hotmail.com>
2016-09-07 04:11:20 -0700

    speech indicator fades

--- prototype1/Assets/Scripts/InteractNotif.cs
+++ prototype1/Assets/Scripts/InteractNotif.cs
@@ -2,28 +2,51 @@
 using System.Collections;
 
 public class InteractNotif : MonoBehaviour {
+
+    public float minimum = 0.0f;
+    public float maximum = 1f;
+    public float fadeSpeed = 5.0f;
+    public float threshold = float.Epsilon;
+
+    public bool faded = false;
+
     public GameObject speechBubble;
+    public SpriteRenderer sprite;
     InterRadiusScript NPC;
 
-    void Start() {
-        NPC = transform.parent.GetComponent<InterRadiusScript>();                     // get parent script
+    void Awake() {
+        NPC = transform.parent.GetComponent<InterRadiusScript>();   // get parent script
     }
 
     void Update() {
         bubbleHandler();
+        StartCoroutine("doFade");
     }
 
     public void bubbleHandler() {
-        //  NPC = GameObject.Find("npc1").gameObject;        // find child gameobject
-
-        if(NPC.playerInteract == true) {
-            foreach (SpriteRenderer speechBubble in GetComponentsInChildren<SpriteRenderer>()) {
-                speechBubble.material.color = new Color(1f, 1f, 1f, 1);
-            }
+        // when player enters interact radius
+        if (NPC.playerInteract == true) {
+            faded = true;
         } else {
-            foreach (SpriteRenderer speechBubble in GetComponentsInChildren<SpriteRenderer>()) {
-                speechBubble.material.color = new Color(1f, 1f, 1f, 0);
+            faded = false;
+        }
+    }
+
+    IEnumerator doFade() {
+        float step = fadeSpeed * Time.deltaTime;
+
+        if (faded) {
+            sprite.color = new Color(1f, 1f, 1f, Mathf.Lerp(sprite.color.a, maximum, step));
+            if (Mathf.Abs(maximum - sprite.color.a) <= threshold) {
+                faded = false;
             }
+            yield return new WaitForSeconds(fadeSpeed);
+        } else {
+            sprite.color = new Color(1f, 1f, 1f, Mathf.Lerp(sprite.color.a, minimum, step));
+            if (Mathf.Abs(sprite.color.a - minimum) <= threshold) {
+                faded = true;    
+            }
+            yield return new WaitForSeconds(fadeSpeed);
         }
     }
 }
