e389abeb5a510be1ddde656062cab20d0e657429
Bak <shaia.bak@hotmail.com>
2016-09-08 04:25:45 -0700

    small adjustments / consistant interact radius size

--- prototype1/Assets/Scripts/InterRadiusScript.cs
+++ prototype1/Assets/Scripts/InterRadiusScript.cs
@@ -5,6 +5,10 @@ public class InterRadiusScript : MonoBehaviour {
 
     public bool playerInteract = false;
 
+    void Awake() {
+        transform.localScale = new Vector3(3f, 3f, 0);  // set scale so all radii are consistant 
+    }
+
     void OnTriggerEnter2D(Collider2D col) {
         // Debug.Log(col.collider.name);
        if (col.name == "player") {
