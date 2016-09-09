41c0dfd7de30f1ad20a9238bdbc1e94e3abdf1da
Bak <shaia.bak@hotmail.com>
2016-09-09 02:55:59 -0700

    Text box and static text set up
    No NPC interaction yet
    
    No loading in text yet.. all very simple

---
+++ prototype1/Assets/Scripts/AnimateText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateText : MonoBehaviour {
+    public AnimateText() { }
+
+    public void animateText(string strComplete) {
+        int i = 0;
+        string str = "";
+        while (i < strComplete.Length) {
+            str += strComplete[i++];
+            new WaitForSeconds(0.5f);
+        }
+    }
+}
---
+++ prototype1/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+    public Text theText;
+
+    public PlayerController player;
+
+    public bool textBoxActive;
+
+	// Use this for initialization
+	void Start () {
+        if (textBoxActive) {
+            enableTextBox();
+        } else {
+            disableTextBox();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyUp(KeyCode.Space)) {
+            if (!textBoxActive) {
+                enableTextBox();
+            } else {
+                disableTextBox();
+            }
+        }
+	}
+
+    public void enableTextBox() {
+        textBox.SetActive(true);
+        textBoxActive = true;
+    }
+
+    public void disableTextBox() {
+        textBox.SetActive(false);
+        textBoxActive = false;
+    }
+}
