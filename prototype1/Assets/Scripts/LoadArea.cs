bd87a379439e9231e209a35eafe248410afcec96
Bak <shaia.bak@hotmail.com>
2016-09-23 01:45:45 -0700

    locked door stuff started?

--- prototype1/Assets/Scripts/LoadArea.cs
+++ prototype1/Assets/Scripts/LoadArea.cs
@@ -6,18 +6,23 @@ public class LoadArea : MonoBehaviour {
 
     public Rigidbody2D playerRigi;
     public string AreaToLoad;
+    public bool locked = false;
 
     IEnumerator OnTriggerEnter2D(Collider2D other) {
+        if(locked == true) {
+            Debug.Log("Locked!!");
+            yield return null;
+        } else {
+            yield return new WaitForSeconds(0.2f);      // pause before loading
 
-        yield return new WaitForSeconds(0.2f);      // pause before loading
-
-        float fadeTime = GameObject.Find("GameController").GetComponent<fadeScene>().BeginFade(1);  // find fadeScene script in GameController; 1 is in BeginFade(1) cuz 1 is fade out and -1 is fade in
-        if (other.gameObject.tag == "Player") {
-            other.gameObject.GetComponent<PlayerController>().canMove = false;
-            playerRigi = other.gameObject.GetComponent<Rigidbody2D>();
-            playerRigi.velocity = Vector2.zero;                                                     // stops player
+            float fadeTime = GameObject.Find("GameController").GetComponent<fadeScene>().BeginFade(1);  // find fadeScene script in GameController; 1 is in BeginFade(1) cuz 1 is fade out and -1 is fade in
+            if (other.gameObject.tag == "Player") {
+                other.gameObject.GetComponent<PlayerController>().canMove = false;
+                playerRigi = other.gameObject.GetComponent<Rigidbody2D>();
+                playerRigi.velocity = Vector2.zero;                                                     // stops player
+            }
+            yield return new WaitForSeconds(fadeTime);                                                  // pause until fade is done  
+            SceneManager.LoadScene(AreaToLoad);                                                         // load new scene
         }
-        yield return new WaitForSeconds(fadeTime);                                                  // pause until fade is done  
-        SceneManager.LoadScene(AreaToLoad);                                                         // load new scene
     }
 }
---
+++ prototype1/Assets/Scripts/PlayerVariables.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerVariables : MonoBehaviour {
+    public bool door_town1Aproto = false;
+    public bool door_world1Aproto = false;
+
+}
